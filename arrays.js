4b8f3a7ca9310ebc8c9840f678379ecf46e0d351
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-09 22:06:36 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(arr){
 };
 
 function removeElementFromEndOfArray(arr){
-  arr.slice(0, arr.length - 1);
+  arr = arr.slice(0, arr.length - 1);
   return arr;
 };
