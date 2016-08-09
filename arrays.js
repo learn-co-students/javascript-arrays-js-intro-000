62c2d79812f9ec1889ff2f0d5440a0c5cf60bc70
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-09 21:56:17 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = [`snickers`, `hundred grand`, `kitkat`,`skittles`];
+
+function addElementToBeginningOfArray(arr, item){
+  arr = [item, ...arr];
+  return arr;
+};
+
+function destructivelyAddElementToBeginningOfArray(arr, item){
+  arr.unshift(item);
+  return arr;
+};
+
+function addElementToEndOfArray(arr, item){
+  arr = [...arr, item];
+  return arr;
+};
+
+function destructivelyAddElementToEndOfArray(arr, item){
+  arr.push(item);
+  return arr;
+};
+
+function accessElementInArray(arr, index){
+  return arr[index];
+};
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.shift();
+  return arr;
+};
+
+function removeElementFromBeginningOfArray(arr){
+  arr = arr.slice(1);
+  return arr;
+};
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  arr.pop();
+  return arr;
+};
+
+function removeElementFromEndOfArray(arr){
+  arr.slice(0, arr.length - 1);
+  return arr;
+};
