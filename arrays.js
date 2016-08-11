7cf623d274ca574d8eb33571d8ee1c2ade9aec08
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-11 19:43:00 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars=["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray (array,element){
+  return [element,...array]
+
+}
+
+function descructivelyAddElementToBeginningOfArray (array,element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray (array,element){
+  var returnArray =[...array, element]
+  return returnArray
+}
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array,index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  var a=array.slice(1)
+  return a
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length-1)
+}
