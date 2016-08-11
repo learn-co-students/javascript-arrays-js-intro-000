2ba3b79bc985757c42ee12c0ee09359debbf1ba7
Karina Bartz <ksbartz@gmail.com>
2016-08-11 16:47:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,41 +1,10 @@
 
-var chocolateBars
+var chocolateBars;
 
-chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-/*
-newelement = 1;
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+newelement = "foo";
 function addElementToBeginningOfArray (newArray, newelement){
-  newArray = [2, 3, 4];
+  newArray = [1];
 };
 [newelement, ...newArray];
-
-
-var destructivelyAddElementToBeginningOfArray = function (var nuArray = ["B", "C", "D"], "A"){
-  return nuArray.unshift("A");
-}
-var addElementToEndOfArray = function (var endArray = ["A","B","C"], "D" ){
-  return [...endArray, "D"];
-}
-var destructivelyAddElementToEndOfArray = function (var enArray = [1,2,3], 4){
-  return enArray.push("4");
-}
-
-var accessElementInArray = function (var ray = ["cats","dogs","mice"]){
-  return (ray[0]);
-}
-
-var destructivelyRemoveElementFromBeginningOfArray = function (const days = ["Monday", "Tuesday", "Wednesday"]){
-  return days.shift();
-}
-
-var removeElementFromBeginningOfArray = function (var cats = ["Milo", "Garfield", "Otis"]){
-  return cats = cats.slice(1);
-}
-
-var destructivelyRemoveElementFromEndOfArray = function (var iceCreams = ["chocolate", "vanilla", "raspberry"]){
-  return iceCreams.pop();
-}
-
-var removeElementFromEndOfArray = function (var daray = ["jack","john","jill"]){
-  return daray.slice(0, daray.length - 1)
-}
