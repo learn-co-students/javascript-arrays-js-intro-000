6b78ff9c851487dd98bfcc7727e1ce3197dab8d8
Karina Bartz <ksbartz@gmail.com>
2016-08-11 16:23:28 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+
+var chocolateBars
+
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+/*
+newelement = 1;
+function addElementToBeginningOfArray (newArray, newelement){
+  newArray = [2, 3, 4];
+};
+[newelement, ...newArray];
+
+
+var destructivelyAddElementToBeginningOfArray = function (var nuArray = ["B", "C", "D"], "A"){
+  return nuArray.unshift("A");
+}
+var addElementToEndOfArray = function (var endArray = ["A","B","C"], "D" ){
+  return [...endArray, "D"];
+}
+var destructivelyAddElementToEndOfArray = function (var enArray = [1,2,3], 4){
+  return enArray.push("4");
+}
+
+var accessElementInArray = function (var ray = ["cats","dogs","mice"]){
+  return (ray[0]);
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = function (const days = ["Monday", "Tuesday", "Wednesday"]){
+  return days.shift();
+}
+
+var removeElementFromBeginningOfArray = function (var cats = ["Milo", "Garfield", "Otis"]){
+  return cats = cats.slice(1);
+}
+
+var destructivelyRemoveElementFromEndOfArray = function (var iceCreams = ["chocolate", "vanilla", "raspberry"]){
+  return iceCreams.pop();
+}
+
+var removeElementFromEndOfArray = function (var daray = ["jack","john","jill"]){
+  return daray.slice(0, daray.length - 1)
+}
