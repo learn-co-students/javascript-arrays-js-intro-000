f17e17befdbf787e655ff628b4e1791b55158508
maxgrok <maxgrok2011@gmail.com>
2016-08-14 21:20:40 -0400

    Done.

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray = (Array, element) =>{
+  //add element to the beginning of the array
+  Array.unshift(element);
+  //return whole array
+  return Array;
+}
+
+function destructivelyAddElementToBeginningOfArray = (Array, element) =>{
+  //add element to the beginning of the array
+  Array.unshift(element);
+  //return whole array
+  return Array;
+}
+
+function addElementToEndOfArray = (array, element)=>{
+
+}
+
+function destructivelyAddElementToEndOfArray =(array, element)=>{
+
+}
