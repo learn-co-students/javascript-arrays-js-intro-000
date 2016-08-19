2098677548cd6e65294603ee3ad5d46ffe53f6c8
Hewan Abreha <henebys@gmail.com>
2016-08-19 05:33:48 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", " kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index) {
+  console.log(array[2]);
+  return array[2]
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array.slice(1)
+}
+
+function removeElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
