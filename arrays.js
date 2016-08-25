606f9151fb212827136fe4a2dfb2ff10fa46f71a
Cindy Gao <cindygao@hotmail.com>
2016-08-25 05:52:08 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element){
@@ -13,7 +14,8 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index){
