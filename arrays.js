dc17323598847d3d1a82cea0f2700fae9b89516e
imani fecu <imani.fecu72@ymail.com>
2016-08-29 02:50:36 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,33 @@
+function addElementToBeginningOfArray(element, array){
+  array=[array, ...element]
+  return array
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+array.unshift(element)
+return array
+}
+function addElementToEndOfArray(array, element){
+  array=[...array, element]
+  return array
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array){
+ return array.slice(0, array.length-1)
+}
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
