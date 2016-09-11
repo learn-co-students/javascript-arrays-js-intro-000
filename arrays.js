ca7f70d285bca3ebb5aa384bab12a08767b957b5
Luis Marte <mluis12@rocketmail.com>
2016-09-11 23:30:18 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,69 @@
+var ingredient1 = "bread";
+var ingredient2 = "mild cheese";
+var ingredient3 = "sharp cheese";
+var ingredient4 = "butter";
+var ingredient5 = "tomato";
+var ingredient6 = "garlic";
+
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var array = array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(0);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(0, array.length -1);
+  return array;
+}
