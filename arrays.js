117c3d9c1c35afd773347a7021f904779bbd57f2
Tayari Appiah <cjhhh.ta@gmail.com>
2016-09-23 21:50:40 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,3 +4,28 @@ function addElementToBeginningOfArray(array, element){
    arrays = [element, ...array]
      return arrays
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+    array.unshift(element)
+      return array
+}
+
+function addElementToEndOfArray(array, element){
+  difArrays = [...array, element]
+    return difArrays
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+      array.push(element);
+        return array
+}
+
+function accessElementInArray(array, index){
+    return array[index];
+
+}
+
+destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+    return array
+}
