2cfe203878a40763566ab50f555a0fb0df2c521c
Andy Cary <andy@andycary.com>
2016-10-07 23:06:21 +0000

    add elements

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = new Array('snickers', 'hundred grand', 'kitkat', 'skittles');
+
+function addElementToBeginningOfArray(array, add_element) {
+    new_array = new array.unshift(add_element);
+    return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, dadd_element) {
+    array.unshift(add_element);
+    return array;
+}
+
+function accessElementInArray() {
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+
+}
+
+function removeElementFromBeginningOfArray() {
+
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+
+}
+
+function removeElementFromEndOfArray() {
+
+}
