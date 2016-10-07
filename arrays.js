e82c0244249316a70142240145593a6dd6975980
Andy Cary <andy@andycary.com>
2016-10-07 23:36:08 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,31 +1,45 @@
 var chocolateBars = new Array('snickers', 'hundred grand', 'kitkat', 'skittles');
 
 function addElementToBeginningOfArray(array, add_element) {
-    new_array = new array.unshift(add_element);
-    return array;
-}
-
-function destructivelyAddElementToBeginningOfArray(array, dadd_element) {
     array.unshift(add_element);
     return array;
 }
 
-function accessElementInArray() {
-
+function destructivelyAddElementToBeginningOfArray(array, dadd_element) {
+    array.unshift(dadd_element);
+    return array;
 }
 
-function destructivelyRemoveElementFromBeginningOfArray() {
-
+function addElementToEndOfArray(array, element_add) {
+    array.push(element_add)
+    return array;
 }
 
-function removeElementFromBeginningOfArray() {
-
+function destructivelyAddElementToEndOfArray(array, element_add) {
+    array.push(element_add)
+    return array;
 }
 
-function destructivelyRemoveElementFromEndOfArray() {
-
+function accessElementInArray(array, index) {
+    return array[index];
 }
 
-function removeElementFromEndOfArray() {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+    array.shift();
+    return array;
+}
 
+function removeElementFromBeginningOfArray(array) {
+    array.shift();
+    return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop();
+    return array;
+}
+
+function removeElementFromEndOfArray(array) {
+    var new_array = array.slice(0, array.length-1);
+    return new_array;
 }
