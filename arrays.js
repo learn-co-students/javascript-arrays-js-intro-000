acc8ce8e6bdb18201540a61b6a8b72217444f793
Andrew Schubert <andrew.schubert1986@gmail.com>
2016-11-09 02:52:13 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,20 +1,42 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(x, y) {
-  var arr = [y]
-  var x = chocolateBars
-  var newChocolateBars = [y, ...x]
+function addElementToBeginningOfArray(arry, element) {
+  var newChocolateBars = [element, ...arry]
+  return newChocolateBars
+};
+
+function destructivelyAddElementToBeginningOfArray(arry, element) {
+  arry.unshift(element)
+  return arry
+};
+
+function addElementToEndOfArray(arry, element) {
+  var newChocolateBars = [...arry, element]
   return newChocolateBars
 }
 
-function destructivelyAddElementToBeginningOfArray(x, y) {
-  var arr = [y]
-  var x = chocolateBars
-  return  y, ...x
+function destructivelyAddElementToEndOfArray(arry, element) {
+  arry.push(element)
+  return arry
 }
 
+function accessElementInArray(arry, index) {
+  return arry[index]
+}
 
+function destructivelyRemoveElementFromBeginningOfArray(arry) {
+  return arry.shift()
+}
 
+function destructivelyRemoveElementFromEndOfArray(arry) {
+  return arry.pop()
+}
 
+function removeElementFromEndOfArray(arry) {
+  var removeLast = arry.slice(0, arry.length - 1)
+  return removeLast
+}
 
-function addElementToEndOfArray() {}
+function removeElementFromBeginningOfArray (arry) {
+  return arry.slice(1)
+}
