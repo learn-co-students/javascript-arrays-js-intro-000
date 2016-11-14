dccacbcb9c95da836d39d86cd8ae4cb9f45fa03b
awowa <sangdencre@gmail.com>
2016-11-14 12:06:12 -0600

    Done.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,38 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-The test wont pass.
+function addElementToBeginningOfArray(array, element) {
+	return [element, ...array]
+}
+
+function destructiveylAddElementToBeginningOfArray(array, element) {
+	return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+	return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+	return array.push(element)
+}
+
+function accessElementInArray(array, index) {
+	return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+	array.shift()
+	return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+	return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+	return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+	return array.slice(0, array.length - 1)
+}
