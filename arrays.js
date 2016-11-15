da28cbb32f435d54041dcc9e2db53868259bece7
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-15 03:10:41 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+
+function addElementToBeginningOfArray (array, element) {
+  const someArray = [];
+  const myOtherArray = ["foo", 1, ...someArray];
+  // should return new array and not modify original
+  return myOtherArray;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  var addToBeginning = [];
+  addToBeginning.unshift("foo", 1);
+  return addToBeginning;
+}
+
+function addElementToEndOfArray (array,element) {
+  const addElementEnd = [];
+  const addElementNew = [...addElementEnd, 1, "foo"];
+  return addElementNew;
+}
+
+function destructivelyAddElementToEndOfArray (array,element) {
+  var someOtherArray = [1, "foo"];
+  someOtherArray.push();
+  return someOtherArray;
+}
+
+
+
+
+function accessElementInArray (array, index) {
+  var access = [1,2,3];
+  return access[2];
+}
+
+
+function removeElementFromBeginningOfArray (array,element){
+  var removeMe = ["el1", "el2"];
+  removeMe = removeFromBeginnig.slice(1);
+  return removeMe;
+}
