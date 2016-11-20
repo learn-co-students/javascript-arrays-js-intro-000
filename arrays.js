16bf6bfa7cd7a48096aa0e307c762dcbaf723831
Luis Alves <lmoalves@gmail.com>
2016-11-20 20:58:00 +0000

    Finished

---
+++ arrays.js
@@ -0,0 +1,51 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+    array = [element, ...array];
+}
+
+function addElementToBeginningOfArray(array, element){
+    array.unshift(element);
+    return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+    array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element) {
+    array = [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element);
+}
+
+function accessElementInArray(array, index){
+    return array[index];
+}
+
+function removeElementFromBeginningOfArray(array){
+    array.shift();
+    return array;
+}
+
+function removeElementFromEndOfArray(array){
+    array.pop();
+    return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+    array.push(element);
+    return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+    array.unshift(element);
+    return array;
+}
+
+function addElementToEndOfArray(array, element){
+    array.push(element);
+    return array;
+}
