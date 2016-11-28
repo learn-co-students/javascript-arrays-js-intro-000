1b13e9870aae9b0dbd8f0c432343204c18d3699c
Ber Cohen <thegreatber@gmail.com>
2016-11-28 21:08:15 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(){var bars=["Roxy"];bars=["sixth",...bars]}
+
+function destructivelyAddElementToBeginningOfArray(){
+
+}
