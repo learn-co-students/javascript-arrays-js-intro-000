97afb5a135fee6307d1a8c84ad60dc8bd1b639d8
Patrick Logan <jpatricklogan@gmail.com>
2016-11-29 22:24:12 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  [...array, element]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+
+}
+
+function addElementToEndOfArray(array, element) {
+  [element, ...array]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+}
+
+function accessElementInArray(array, index) {
+  array[index];
+}
