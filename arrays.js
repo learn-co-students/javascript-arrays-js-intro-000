0c3f074b66214c2c6ca5d1da5572356a7466480d
Stuart Hahn <slumpcity@outlook.com>
2016-11-29 03:21:24 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -6,33 +6,33 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
-  var array = [element, ...array];
-  return array;
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
+  array.unshift(element)
+
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-  var array = [...array, element];
-  return array;
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
+  array.push(element)
+
+  return array
 }
 
 function accessElementInArray(array, index) {
-  return array[index];
+  return array[index]
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1);
+  return array.slice(1)
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1);
+  return array.slice(0, array.length - 1)
 }
