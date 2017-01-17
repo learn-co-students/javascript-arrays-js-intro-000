eabc451fefaba18f14d5f2f9da8fbfe72a0d5e2a
Jim Tryon <j.tryon@outlook.com>
2017-01-17 09:59:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,3 +10,14 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  array.push(element);
+  var array = [...array];
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
