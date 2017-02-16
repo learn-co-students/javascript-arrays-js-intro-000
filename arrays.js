a7966e868e23a0b62188a90d655a43ddfe64eb76
Asher Shaheen <ashershaheen@gmail.com>
2017-02-16 13:54:33 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]
+
+function addElementToBeginningOfArray (Array, Element) {
+  addElementToBeginningOfArray.unshift("Element")
+  
+  
+}
+
