c7bc24d794ebe4c93b958dc50a619453d80e4ca1
Jasmine Greene <jasmine.greene7@gmail.com>
2017-02-18 01:04:48 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray (array, element)
+{
+  var array = "foo"
+  var element = 1
+  return [array,element]
+  }
+  function destructivelyAddElementToBeginningOfArray (array, element)
+  {
+    return array, element
+  }
