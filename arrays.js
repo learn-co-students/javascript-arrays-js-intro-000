e0018f80d0455488d3727bd8b5a4f0fd0656b224
louis baptiste <louisvis@ymail.com>
2017-02-23 04:48:45 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+
+var chocolatebars = ["snickers","hundred grand", "kitkat", "skittles"];
+
+var array1 = [""];
+var beginningofarray= [string, ...array1]
+function addElementToBeginningOfArray (array1, beginningofarray )
+{
+  return array1
+}
