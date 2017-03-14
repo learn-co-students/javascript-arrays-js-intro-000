b1839f8d816e740c3af7c1d3a36f97dc3b639f26
Ricardo Medley <r.medley010306@gmail.com>
2017-03-14 03:44:15 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers","hundred grand","kit kat","skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  var array = []
+  return array.unshift(element);
+}
