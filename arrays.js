b22508d835a15a0f4ba765cbc18d9a65cfa72c82
Anthony Grimes <agrimes@charter.net>
2017-03-20 00:48:13 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, ele) {
+    var newArr = [ele, ...arr];
+    return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, ele) {
+    arr.unshift(ele);
+    return arr;
+}
+
+function addElementToEndOfArray(arr, ele) {
+    var newArr = arr;
+    newArr.push(ele);
+    return newArr;
+}
+
+function destructivelyAddElementToEndOfArray(arr, ele) {
+    arr.push(ele);
+    return arr;
+}
+
+function accessElementInArray(arr, index) {
+    return arr[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+    arr.shift();
+    return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+    var newArr = arr.slice(1);
+    return newArr;
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+    arr.pop();
+    return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+    var newArr = arr.slice(0, arr.length  - 1);
+    return newArr;
+}
