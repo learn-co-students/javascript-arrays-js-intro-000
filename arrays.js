3e342d02267d8d091308053f90dfb259c6d14faf
Taumata Rolleston <taumata21@gmail.com>
2017-03-24 01:58:23 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+var addElementToBeginnningOfArray = [1]
+const array = [1]
+['foo', ...addElementToBeginnningOfArray]
+
+var destructivelyAddElementToBeginningOfArray = [1]
+destructivelyAddElementToBeginningOfArray.unshift('foo');
