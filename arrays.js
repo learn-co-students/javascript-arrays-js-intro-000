af97dcd6de452de106e2e4011ce441e9d83991de
Manraj Parmar <manrajp@gmail.com>
2017-03-24 01:47:03 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ["snickers", "hundred grad", "kitkat", "skittles"];
+
+/* function addElementToBeginningOfArray(array, element) {
+  [element, ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(myArray, myElement) {
+   myArray.unshift(myElement);
+   return myArray
+}
+
+function addElementToEndOfArray(myArray, myElement) {
+  return [...myArray, myElement]
+}
+
+function destructivelyAddElementToEndOfArray(myArray, myElement) {
+  myArray.push(myElement)
+  return myArray
+}
+
+function accessElementInArray(myArray, myIndex) {
+  return myArray[myIndex];
+}
+
+function destructivelyRemoveElementFromEndOfArray(myArray) {
+  myArray.shift()
+  return myArray;
+}
+
+function removeElementFromBeginningOfArray(myArray) {
+  myArray.slice(1);
+  return myArray;
+}
+
+fuction destructivelyRemoveElementFromEndOfArray(myArray) {
+  myArray.pop();
+  return myArray
+}
+
+function removeElementFromEndOfArray(myArray) {
+  myArray.slice(0, myArray.length - 1)
+  return myArray;
+} */
