dae051721bd5adf48df843dc5ee7fb916a3cb81d
Adel R <r-27adel@live.fr>
2017-05-26 15:05:11 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+
+  newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element)
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(-1)
+}
