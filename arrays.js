4b3b258d6cd1f62c0cb918ade05d28a5b525291f
David Plata <dplata83@gmail.com>
2017-05-28 23:36:00 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,24 +1,52 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+/////////////////////////////////////////////
 
-
-
-
-
-
-function addElementToBeginningOfArray() {
-
-}
-
-function destructivelyAddElementToBeginningOfArray() {
-
+function addElementToBeginningOfArray(array, element) {
+  return([element, ...array]);
 }
 
 
-function adElementToEndOfArray() {
 
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
 }
 
-function destructivelyAddElementToEndOfArray() {
+//////////////////////////////////////////////
 
+function addElementToEndOfArray(array, element) {
+  return([...array, element]);
 }
+
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+////////////////////////////////////////////////////
+
+function accessElementInArray(array, index) {
+  return([1, 2, 3], 3);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift([0]);
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+}
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1);
+}
+
+//////////////////////////////////////////////////////
+
+//function destructivelyAddElementToBeginningOfArray(array) {
+//  array.shift();}
