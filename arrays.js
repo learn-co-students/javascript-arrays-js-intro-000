4c462a4ccd2245ca93e5e1fce2d1f8ab5485f1ac
Jagat Tuladhar <jagatjyoti.1si13cs040@gmail.com>
2017-06-07 13:19:11 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+//var chocolateBars = new Array();
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var array = new Array();
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+}
