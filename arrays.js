e769a0e674f15ec361c83d50417e3f82aa9bde4a
sumanth Jesta <sumanthjesta14@gmail.com>
2017-07-02 14:40:25 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,6 +1,8 @@
 var chocolateBars=["snickers", "hundred grand","kitkat","skittles"];
 var a=[1,2,3];
 var b=4;
+var ind=[10,20,30,40];
+var i=2;
 function addElementToBeginningOfArray(a,b)
 {
   var c=[b,...a];
@@ -15,7 +17,7 @@ function destructivelyAddElementToBeginningOfArray(a,b)
 
 function addElementToEndOfArray(a,b)
 {
-  var c=[a,...b];
+  var c=[...a,b];
   return c;
 }
 
@@ -23,4 +25,35 @@ function destructivelyAddElementToEndOfArray(a,b)
 {
   a.push(b)
   return a;
+}
+
+function accessElementInArray(ind,i)
+{
+  return ind[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a)
+{
+  a.shift();
+  return a;
+}
+
+function removeElementFromBeginningOfArray(a)
+{
+  var c=a.slice(1);
+  return c;
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(a)
+{
+  a.pop();
+  return a;
+}
+
+
+function removeElementFromEndOfArray(a)
+{
+  var c=a.slice(0,a.length-1);
+  return c;
 }
