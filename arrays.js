dc5d5f198c28a69f94df9ef1c6508a5e66b621a0
Chinwe Vivian Ononiwu <ononiwu01@gmail.com>
2017-07-07 22:14:26 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray (array, element) {
+  return [ 'foo', 1]
+
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  return array.unshift[ 'foo', 1]
+
+}
