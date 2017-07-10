2d583eb16b7abe175cf622ffca09d7d0abf390d6
Ahmad Abdullah Tariq <ahmadabdullah247@live.com>
2017-07-10 02:11:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array,element){
-  return [element,..,array];
+  return [element,...,array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
