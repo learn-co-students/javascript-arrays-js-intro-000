a81a17f574eb7df1f5825375577a2a0c09e08ccf
Lauren Day <laurendaysmith@gmail.com>
2017-07-18 13:55:58 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -30,16 +30,16 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array = array.slice(1);
-  return array;
+  return array.slice(1);
+
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
-  return array;
+  array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array){
-  array = array.slice(0, array.length - 1);
-  return array;
+  return array.slice(0, array.length - 1)
+
 }
