2902f72f0a1e9938516daf4fe1d6774f84d49998
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-24 03:03:28 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -23,3 +23,37 @@ function destructivelyAddElementToEndOfArray(array, element) {
     array.push(element);
     return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[2];
+}
+
+function destructivelyRemoveElementFromBegginingOfArray(array) {
+  array.slice(0);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop(2);
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+array.pop();
+return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
