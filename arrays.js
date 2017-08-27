fcfcc768e4b2747efb7446ab010bea9277726fd5
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-27 12:28:33 -0400

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+	return array.push(element)
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
+
+function addElementToEndOfArray(array, element) {
+
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+
+}
+
+function accessElementInArray(array, index){
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+
+}
+
+function removeElementFromBeginningOfArray(array){
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+
+}
+
+function removeElementFromEndOfArray(array){
+
+}
