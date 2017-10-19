203cc6d7960620f2bfab79132105788d4ccc57b0
Eric Sherman <esherman9@gmail.com>
2017-10-19 05:03:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,6 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 
 function addElementToEndOfArray(array,element){
   array.push(element)
-  return array
 };
 
 function destructivelyAddElementToEndOfArray(array,element){
