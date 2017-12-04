3d069b9740aa207de46f2e7c07f1a84aca7a73c0
Cory West-Forbes <cwestforbes@gmail.com>
2017-12-04 12:10:32 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,3 +5,50 @@ function addElementToBeginningOfArray (array, element) {
   newArray = [element, ...newArray];
   return newArray;
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var newArray = array;
+  newArray.unshift(element);
+  return newArray;
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = array;
+  newArray = [...newArray, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var newArray = array;
+  newArray.push(element);
+  return newArray;
+}
+
+function accessElementInArray(array, index) {
+  var newArray = array;
+  return newArray[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  var newArray = array;
+  newArray.shift();
+  return newArray;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array;
+  newArray = newArray.slice(1);
+  return newArray;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var newArray = array;
+  newArray.pop();
+  return newArray;
+}
+
+function removeElementFromEndOfArray(array) {
+  var newArray = array;
+  newArray = newArray.slice(0, array.length - 1);
+  return newArray;
+}
