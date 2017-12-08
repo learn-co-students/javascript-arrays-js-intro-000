e9ff3e7eab80f7292ac9af942317cbbf45e49bf1
David Clelland <davidclelland6@gmail.com>
2017-12-08 00:05:12 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -20,3 +20,34 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 destructivelyAddElementToBeginningOfArray([2, 3], 1);
+
+function addElementToEndOfArray (array, element){
+  var newArray = array.slice();
+  newArray.push(element);
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray (array, element){
+  var newArray = array;
+  newArray.push(element);
+  return newArray;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var newArray = array.slice(1);
+  return newArray;
+
+}
+
+
+
