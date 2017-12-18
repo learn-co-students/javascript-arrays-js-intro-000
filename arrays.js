946dc2964a3e8e2a037a53569b7aaf775d3dc00a
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-18 14:50:38 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element);
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+}
