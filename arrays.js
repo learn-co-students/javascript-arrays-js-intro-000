41491bb852b363b106b00ec07385d904bc327f5e
mark melesse <mark.melesse@gmail.com>
2018-01-21 22:22:46 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+   var newArray = array.slice()
+   newArray.unshift(element);
+   return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+   array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = array.slice()
+  newArray.push(element)
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array;
+}
+
+function accessElementInArray(array, index) {
+ var array = array[index];
+ return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+
+}
