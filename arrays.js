83d5bfecc5b9fe0cf3c429e26dc481c4337fefb9
Simone D <monatru3@hotmail.com>
2018-01-30 03:44:33 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars= ["snickers", "hundred grand", "kit kat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+
+return [element,...array]
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift(element)
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var newArray=array.slice(1)
+  return newArray
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  var newArray=array.slice(0,array.length-1)
+  return newArray
+}
