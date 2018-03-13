035179b0087625e1a5b7e71a9b41bd91cbb9cfa3
Liangtian Zhang <lzhang40@binghamton.edu>
2018-03-13 16:57:09 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,26 @@
+function addElementToBeginningOfArray(arr, el) {
+  return [el, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, el) {
+  arr.push(el)
+  return arr
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift
+  return arr
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  return arr.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop
+  return arr
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0,arr.length - 1)
+}
