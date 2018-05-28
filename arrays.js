7895e38cb3386b6e51af78513494792b5ffd3cc6
Joaquin Carbonara <joaquin.o.carbonara@gmail.com>
2018-05-28 12:48:58 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,31 +1,74 @@
-function addElementToBeginningOfArray(arry,elmt){
+var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"]
 
-return arry.unshift(elmt)
+
+function addElementToBeginningOfArray(arry,elmt){
+var newarry
+
+newarry=[elmt, ...arry]
+
+return newarry
 
 }
 
 
 
 function destructivelyAddElementToBeginningOfArray(arry,elmt){
+
+arry.unshift(elmt)
+
+return arry
+
+}
+
+function addElementToEndOfArray(arry,elmt){
 var newarry
 
-newarry = [elmt, ...arry]
+newarry=[...arry, elmt]
 
 return newarry
 
 }
 
-function addElementToEndOfArray(arry,elmt){
 
-return arry.push(elmt)
+function destructivelyAddElementToEndOfArray(arry,elmt){
+
+arry.push(elmt)
+
+return arry
 
 }
 
+function accessElementInArray(arry,indx){
 
-function destructivelyAddElementToEndOfArray(arry,elmt){
+  return arry[indx]
+}
+
+
+function destructivelyRemoveElementFromBeginningOfArray(arry){
+
+arry.shift()
+
+  return arry
+}
+
+function removeElementFromBeginningOfArray(arry){
+  var newarry
+  newarry=arry.slice(1)
+  return newarry
+}
+
+function destructivelyRemoveElementFromEndOfArray(arry){
+
+arry.pop()
+
+return arry
+
+}
+
+function removeElementFromEndOfArray(arry){
+
 var newarry
-
-newarry = [...arry, elmt]
+newarry=arry.slice(0,arry.length - 1)
 
 return newarry
 
