decb04aa5a476f157b96f240e3e218ea7da3b6b1
Eric Mei <shizizerick@aol.com>
2018-06-05 22:12:26 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,3 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var addElementToBeginningOfArray = ["alpha", "bravo", "charlie", "delta"]; ["echo", ... addElementToBeginningOfArray ];  => {}
+var destructivelyAddElementToBeginningOfArray = ["alpha", "bravo", "charlie", "delta"]; destructivelyAddElementToBeginningOfArray.push("echo");
