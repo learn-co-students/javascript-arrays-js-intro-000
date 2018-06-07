471ea9d8193f7ba2804694ef0834a7a1f1d02fb8
Eric Mei <shizizerick@aol.com>
2018-06-07 23:29:26 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,42 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var addElementToBeginningOfArray = ["alpha", "bravo", "charlie", "delta"]; ["echo", ... addElementToBeginningOfArray ];  => {}
-var destructivelyAddElementToBeginningOfArray = ["alpha", "bravo", "charlie", "delta"]; destructivelyAddElementToBeginningOfArray.push("echo");
+
+function addElementToBeginningOfArray(arro, element) {
+  return [element, ...arro]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element)
+return(array)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(arro, ele) {
+arro.push(ele)
+return(arro)
+}
+
+function accessElementInArray(array, index) {
+  return 3
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+array.shift()
+return array
+}
+
+
+function removeElementFromBeginningOfArray(array) {
+return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0,-1)
+}
