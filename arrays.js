ae4ecd0f319a101c3f734f260f973dd188558cd6
Christine Xiang <yutingcxiang@gmail.com>
2018-06-14 18:15:27 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
