85999e03d7d04e3bb9c7e25198e0349550a222a8
Eugenio Gonzalez <e.gzzart@gmail.com>
2018-09-18 19:53:24 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(arr, data) {
+  return [data, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, data){
+  arr.unshift(data)
+  return arr
+}
+
+function addElementToEndOfArray(arr, data){
+  return [...arr, data]
+}
+
+function destructivelyAddElementToEndOfArray(arr, data){
+  arr.push(data)
+  return arr
+}
+
+function accessElementInArray(arr, i){
+  return arr[i]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.shift()
+  return arr
+}
+
+function removeElementFromBeginningOfArray(arr){
+  return arr.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  arr.pop()
+  return arr
+}
+
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0, arr.length-1)
+}
