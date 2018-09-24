279478f4560bd8ee20a477fcfa31af4bbf4b5fd5
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-24 18:52:10 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -32,3 +32,12 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1)
+}
