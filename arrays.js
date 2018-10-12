6c8880f5a9f0fde64dc4aadf9aef20b210cb8947
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-12 15:52:44 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -41,6 +41,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function removeElementFromEndofArray(array) {
+function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
