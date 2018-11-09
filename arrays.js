9ec75e330c5c16a3165ef8a5d8fd808d867699bc
andy reyes <11710reyes@gmail.com>
2018-11-09 04:36:21 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -18,3 +18,25 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
+
+function accessElementInArray (array, index){
+  return array[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
