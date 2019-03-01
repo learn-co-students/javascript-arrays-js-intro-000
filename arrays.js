eabe5b1f5ca8db4a54b7ab27a6f1eab19749d320
Stephen Wool <stevewool7@gmail.com>
2019-03-01 11:48:45 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
@@ -13,7 +14,8 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
@@ -22,18 +24,20 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
+  return array 
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length -1)
+  return array.slice(0, array.length -1)
 }
 
 
