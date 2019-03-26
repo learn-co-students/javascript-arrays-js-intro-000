f7051bcb2670dfbb3f8ef83782e735d6be17219b
Tanuka Das <das.tanuka@yahoo.com>
2019-03-26 13:06:24 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars,"txix" ){
+  chocolateBars = [...chocolateBars, "txix"];
+  //chocolateBars.unshift("txix");
+
+}
+chocolateBars
+function destructivelyAddElementToBeginningOfArray(chocolateBars, "hersey"){
+  //chocolateBars = [...chocolateBars, "hersey"];
+  chocolateBars.unshift("txix");
+}
+chocolateBars;
