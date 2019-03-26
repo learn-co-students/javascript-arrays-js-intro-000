8c73d9963db28e036bd12ef86d4f9cc9e6e4f8a5
Tanuka Das <das.tanuka@yahoo.com>
2019-03-26 18:06:59 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,13 +1,53 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(chocolateBars,"txix" ){
-  chocolateBars = [...chocolateBars, "txix"];
-  //chocolateBars.unshift("txix");
 
+function addElementToBeginningOfArray(chocolateBars, element ){
+  //should return a new array and not modify the original
+  var chocolateBars = [element, chocolateBars[0]]
+  return chocolateBars;
+  //return [ element, ...chocolateBars];
 }
-chocolateBars
-function destructivelyAddElementToBeginningOfArray(chocolateBars, "hersey"){
-  //chocolateBars = [...chocolateBars, "hersey"];
-  chocolateBars.unshift("txix");
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
+//should alter the original array that's passed in
+  chocolateBars.unshift(element);
+  return chocolateBars;
 }
-chocolateBars;
+
+function addElementToEndOfArray(chocolateBars, element){
+//should not alter the original array
+  //array.push(element);                  //push places an element at the end of an array list
+  //return array;
+  return [...chocolateBars, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+//should alter the original array
+  array.push(element);
+  return array;
+
+  //[...array, "element"]
+}
+function accessElementInArray(array, index){
+
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift() ;
+  return array;
+}
+function removeElementFromBeginningOfArray(array){
+  //return the entire array in the same line, and it should not mutate the original array.
+  array.slice(1);
+  return array;
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  // Your function should return the entire array, and it should mutate the array
+  array.pop();
+  return array;
+}
+function removeElementFromEndOfArray(array){
+  //function should return the array without the last element, and it should not mutate the original array.
+  return array.slice(0, array.length - 1);
+  //return array;
+}
