5fb2415af9973f534c30630c1f2a7bf476ca493b
Tara Wright <wrightt2015@gmail.com>
2019-06-02 19:46:33 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+
+function addElementToBeginningOfArray (fruits){
+  moreFruits = ...fruits;
+  return fruits;
+}
+addElementToBeginningOfArray();
+
+function destructivelyAddElementToBeginningOfArray(shapes, moreShapes){
+  var shapes = ["square","triangle","circle"];
+  var moreShapes = shapes.unshift("oval");
+
+  return shapes;
+}
