c3c5c0a563f7844f8de2b8ac18bfcda07d5286c5
Nobuhiko Itakura <n.itakura22@gmail.com>
2019-07-31 11:30:02 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -37,6 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, -1)
+  return array.slice(0, array.length -1)
 
 }
