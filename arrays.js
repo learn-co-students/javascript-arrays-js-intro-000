2cb694c97c5441565c8d0eb64adacb962b613f95
Lucian Amos <locamos@gmail.com>
2019-11-11 20:16:03 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,58 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = [element, ...array];
+  return newArray;
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  // array = [element, ...array]; <--- this doesn't work?
+  return array;
+}
+
+
+function addElementToEndOfArray(array, element){
+  var newArray = [...array, element];
+  return newArray;
+}
+
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  // array [...array , element]; <--- this doesn't work?
+  return array;
+}
+
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+
+function  destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+
+function removeElementFromBeginningOfArray(array){
+  var newArray = array.slice(1);
+  return newArray;
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(0, array.length-1);
+  return newArray;
+}
+
+
