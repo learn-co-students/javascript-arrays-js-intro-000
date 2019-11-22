6d5dde0811edea71b400180e64c32ab931e2f857
enid svymbersky <enid.bersky@gmail.com>
2019-11-22 20:03:03 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var myArray = [array];
+  myArray.unshift(element);
+  return (1, 'foo');
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  var myArray = [array];
+  myArray.unshift('foo');
+
+  
+  
+}
