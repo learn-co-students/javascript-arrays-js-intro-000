777784f8ccd0c17b2b2e7c4a25816fbad7605064
Mary Kang <mary.kang09@gmail.com>
2020-02-12 05:22:12 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array,element){
+  var array = [1];
+  array = ["foo",...array];
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift('foo');
+  return array;
+}
+
+function addElementToEndOfArray(array,element){
+  var array = [1];
+  array = [...array,"foo"];
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push('foo');
+  return array
+}
+
+function accessElementInArray(array,index){
+  var array = [3];
+  return array [0];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var array = [1,2,3];
+  array = array.slice(1);
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray (array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var array = [1,2,3];
+  array = array.slice(0, array.length - 1);
+  return array
+}
