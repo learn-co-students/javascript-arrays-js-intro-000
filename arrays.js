1c55d586af9cd536949a02eaa44546fe1a519334
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-24 17:21:34 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,8 +1,42 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
 var addElementToBeginningOfArray = (array, element) => {
-  var newArr = [...element]
-  return newArr;
+  return [element, ...array]
 }
 
 var destructivelyAddElementToBeginningOfArray = (array, element) => {
-  return array.unshift(element);
+  array.unshift(element);
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newArr = [...array, element]
+  return newArr;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+var accessElementInArray = (array, index) => {
+  return array[index];
+}
+
+var  destructivelyRemoveElementFromBeginningOfArray = (array) => {
+  array.shift();
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array
+}
+
+var removeElementFromEndOfArray = (array) => {
+  return array.slice(0, array.length -1)
 }
