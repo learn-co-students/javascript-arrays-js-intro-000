eb84581671594d416741f180106b0f208a9db4dc
Steven Quach <stevendquach@gmail.com>
2020-08-20 08:05:15 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -23,5 +23,16 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift(0)
   return array
-
+}
+function removeElementFromBeginningOfArray(array) {
+    var i = array.slice(1)
+    return i
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  var y = array.slice(0, array.length -1)
+  return y;
 }
