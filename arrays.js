4523ea0382754ac388ef90bf648843cf13ff051a
Louis Michael Medina <viewsdevelop@gmail.com>
2020-12-01 15:54:00 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  return array.slice(0)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
